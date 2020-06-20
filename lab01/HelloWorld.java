f3df5cf91702024e39b35e57ab7544c39d4a4bff
simplemj <47730656+simplemj@users.noreply.github.com>
2020-06-20 16:15:14 +1000

    Add files via upload

---
+++ lab01/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+* A simple Java Program that prints a hellp world message to the console
+*
+*/
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to Java");
+    }
+}
+
